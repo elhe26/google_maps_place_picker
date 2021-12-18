45cf8ff04ac37fca512a5843d1bd6b99ca51eb6f
Erick <erick.heredia@gmail.com>
2021-12-18 03:05:20 -0400

    Changing example code.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -107,43 +107,55 @@ class _HomePageState extends State<HomePage> {
                           onPlacePicked: (result) {
                             selectedPlace = result;
                             Navigator.of(context).pop();
-                            setState(() {});
                           },
+                          customBarWidgetBuilder: ElevatedButton(
+                            child: Text("Press"),
+                            onPressed: () {},
+                          ),
                           //forceSearchOnZoomChanged: true,
                           //automaticallyImplyAppBarLeading: false,
                           //autocompleteLanguage: "ko",
                           //region: 'au',
                           //selectInitialPosition: true,
-                          // selectedPlaceWidgetBuilder: (_, selectedPlace, state, isSearchBarFocused) {
-                          //   print("state: $state, isSearchBarFocused: $isSearchBarFocused");
-                          //   return isSearchBarFocused
-                          //       ? Container()
-                          //       : FloatingCard(
-                          //           bottomPosition: 0.0, // MediaQuery.of(context) will cause rebuild. See MediaQuery document for the information.
-                          //           leftPosition: 0.0,
-                          //           rightPosition: 0.0,
-                          //           width: 500,
-                          //           borderRadius: BorderRadius.circular(12.0),
-                          //           child: state == SearchingState.Searching
-                          //               ? Center(child: CircularProgressIndicator())
-                          //               : RaisedButton(
-                          //                   child: Text("Pick Here"),
-                          //                   onPressed: () {
-                          //                     // IMPORTANT: You MUST manage selectedPlace data yourself as using this build will not invoke onPlacePicker as
-                          //                     //            this will override default 'Select here' Button.
-                          //                     print("do something with [selectedPlace] data");
-                          //                     Navigator.of(context).pop();
-                          //                   },
-                          //                 ),
-                          //         );
-                          // },
-                          // pinBuilder: (context, state) {
-                          //   if (state == PinState.Idle) {
-                          //     return Icon(Icons.favorite_border);
-                          //   } else {
-                          //     return Icon(Icons.favorite);
-                          //   }
-                          // },
+                          selectedPlaceWidgetBuilder:
+                              (_, result, state, isSearchBarFocused) {
+                            print(
+                                "state: $state, isSearchBarFocused: $isSearchBarFocused");
+                            return isSearchBarFocused
+                                ? Container()
+                                : FloatingCard(
+                                    bottomPosition:
+                                        0.0, // MediaQuery.of(context) will cause rebuild. See MediaQuery document for the information.
+                                    leftPosition: 0.0,
+                                    rightPosition: 0.0,
+                                    width: 500,
+                                    borderRadius: BorderRadius.circular(12.0),
+                                    child: state == SearchingState.Searching
+                                        ? Center(
+                                            child: CircularProgressIndicator())
+                                        : ElevatedButton(
+                                            child: Text("Pick Here"),
+                                            onPressed: () {
+                                              // IMPORTANT: You MUST manage selectedPlace data yourself as using this build will not invoke onPlacePicker as
+                                              //            this will override default 'Select here' Button.
+                                              setState(() {
+                                                selectedPlace = result;
+                                              });
+
+                                              print(
+                                                  "do something with [${selectedPlace?.geometry ?? 0}] data");
+                                              Navigator.of(context).pop();
+                                            },
+                                          ),
+                                  );
+                          },
+                          pinBuilder: (context, state) {
+                            if (state == PinState.Idle) {
+                              return Icon(Icons.favorite_border);
+                            } else {
+                              return Icon(Icons.favorite);
+                            }
+                          },
                         );
                       },
                     ),
